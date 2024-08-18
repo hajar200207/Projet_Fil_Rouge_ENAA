068432936267c11e7660cd506951878bb189e6d8
hajar ouadi <ouadihajar2002@gmail.com>
2024-08-18 16:02:00 +0100

    add structure de projet

---
+++ conference-management/src/main/java/com/conferencemgmt/conference_management/ConferenceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.conferencemgmt.conference_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceManagementApplication.class, args);
+	}
+
+}
